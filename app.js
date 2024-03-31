9c2a9a4699b148752ef6766b57c2fa0f7cc95883
root <root@ip-172-31-39-37.eu-north-1.compute.internal>
2024-03-31 08:54:58 +0000

    new chanegs

--- app.js
+++ app.js
@@ -2,13 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.get('/status', (req, res) => {
-  res.send('Pipeline working good');
-});
+app.get('/', (req, res) => res.send('Hello World! Pipeline working good'));
 
 app.listen(port, () => {
   console.log(`App running on http://localhost:${port}`);
