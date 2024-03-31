b3de6223c0642d74d9a97701df9189f869a1229e
root <root@ip-172-31-39-37.eu-north-1.compute.internal>
2024-03-31 08:49:41 +0000

    new changes

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+app.get('/status', (req, res) => {
+  res.send('Pipeline working good');
+});
+
+app.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`);
+});
+
